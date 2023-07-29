35ce60d652ddb80d05430b0d8204e570a2e2fa82
Tushar Anekar <anekar.tushar@gmail.com>
2023-07-29 18:33:09 +0530

    feat- video listing component added

---
+++ src/components/VideoListingCard/VideoListingCard.jsx
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import "./videoListing.css"
+import { VideosContext } from "../../contexts/videosContext";
+import { useNavigate } from "react-router-dom";
+import { CategoriesContext } from "../../contexts/categoriesContext";
+
+export function VideoListingCard({ video }) {
+  const { _id, title, views, thumbnail, src, category, creator } = video;
+
+  const navigate = useNavigate()
+  const {addVideoToWatchLater} = useContext(VideosContext)
+  const {categoriesState: { selectedCategory },} = useContext(CategoriesContext)
+
+  function handleWatchLater () {
+    addVideoToWatchLater(video)
+  }
+
+  return (
+    <div className="video-listing-card-container">
+      <iframe src={src}  title={title}></iframe>
+
+      <div className="video-details-container">
+        <img src={thumbnail} alt={title} />
+        <div>
+          <p className="video-title" onClick={() => navigate(`/${selectedCategory.category.toLowerCase()}/${_id}`)}>
+            <strong>{title}</strong>
+          </p>
+          <p>{category}</p>
+          <p>
+            <span>{views} views | </span>
+            <span>{creator}</span>
+          </p>
+        </div>
+      </div>
+      <button onClick={handleWatchLater}>Watch Later</button>
+    </div>
+  );
+}
