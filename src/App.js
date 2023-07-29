5ade0717062abd7451795e2dfd17230e69fd3e28
Tushar Anekar <anekar.tushar@gmail.com>
2023-07-29 18:31:06 +0530

    feat- routes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { Home } from "./pages/Home/Home";
+import { Navbar } from "./components/Navbar/Navbar";
+import { VideoListing } from "./pages/VideoListing/VideoListing";
+import { Video } from "./pages/Video/Video";
+import { WatchLater } from "./pages/WatchLater/WatchLater";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="main-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/:categoryName" element={<VideoListing />}></Route>
+          <Route path="/:categoryName/:videoId" element={<Video />}></Route>
+          <Route path="/watch-later" element={<WatchLater />}></Route>
+
+        </Routes>
+      </div>
     </div>
   );
 }
