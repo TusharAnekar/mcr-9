2f653998701b81b440a44270488c3167c07c9325
Tushar Anekar <anekar.tushar@gmail.com>
2023-07-29 19:41:40 +0530

    feat - note add and delete

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./video.css";
 import { VideosContext } from "../../contexts/videosContext";
@@ -9,16 +9,36 @@ import NotesIcon from "@mui/icons-material/Notes";
 export function Video() {
   const { videoId } = useParams();
   const {
-    videosState: { videos },
+    videosState: { videos, notes },
     addVideoToWatchLater,
+    addNotes,
+    deleteNote,
   } = useContext(VideosContext);
+
+  const [showNoteModal, setShowNoteModal] = useState(false);
+  const [inputNote, setInputNote] = useState("");
 
   const video = videos?.find(({ _id }) => _id === Number(videoId));
 
-  const { _id, title, views, chips, thumbnail, src, category, creator } = video;
+  const { title, thumbnail, src } = video;
 
   function handleWatchLater() {
     addVideoToWatchLater(video);
+  }
+
+  function handleNotes() {
+    setShowNoteModal(true);
+  }
+
+  function handleAddNote() {
+    if (inputNote.length) {
+      addNotes(inputNote);
+      setInputNote("");
+    }
+  }
+
+  function handleDelete(note) {
+    deleteNote(note);
   }
 
   return (
@@ -34,12 +54,31 @@ export function Video() {
         <div className="icons">
           <WatchLaterIcon className="icon" onClick={handleWatchLater} />
           <PlaylistAddIcon className="icon" />
-          <NotesIcon className="icon" />
+          <NotesIcon className="icon" onClick={handleNotes} />
         </div>
       </div>
 
+      {showNoteModal && (
+        <div>
+          <input
+            type="text"
+            value={inputNote}
+            onChange={(e) => setInputNote(e.target.value)}
+          />
+          <button onClick={handleAddNote}>Add Note</button>
+        </div>
+      )}
+
       <div>
         <h3>My Notes</h3>
+        <div>
+          {notes?.map((note, index) => (
+            <div>
+              <p key={index}>{note}</p>
+              <button onClick={() => handleDelete(note)}>Delete</button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
